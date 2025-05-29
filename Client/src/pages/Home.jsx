890843889153d6a516441a60d592bab3f2c481be
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2025-05-29 09:38:04 +0530

    dc

---
+++ Client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+const Home=()=>{
+    return(
+        <>
+        <Navbar/>
+        <h1>thi is a demo</h1>
+        </>
+    )
+}
+
+
+export default Home;
