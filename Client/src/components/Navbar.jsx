34ee098f351a4bdbb798684d4b186b82f5ff8cc0
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2025-05-29 19:32:07 +0530

    doing work on home page

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,8 +1,62 @@
+import { useState } from "react";
 const Navbar=()=>{
+
+    const [isOpen, setIsOpen] = useState(false);
     return(
-        <nav>
-            
-        </nav>
+         <nav className="bg-white ">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center h-22">
+          <div className="text-2xl font-bold text-indigo-600">LogicLab</div>
+
+          {/* Desktop Menu */}
+          <div className="hidden md:flex space-x-6">
+            <a href="#" className="text-gray-700 hover:text-indigo-600 hover:underline">
+               All Blogs
+            </a>
+            <a href="#" className="text-gray-700 hover:text-indigo-600 hover:underline">
+              About
+            </a>
+            <a href="#" className="text-gray-700 hover:text-indigo-600 hover:underline">
+              Contact
+            </a>
+          </div>
+ 
+          <div className="hidden md:block">
+            <button className="bg-indigo-600 text-white px-4 py-2 hover:bg-indigo-700 transition cursor-pointer font-semibold rounded">
+              Get Started
+            </button>
+          </div>
+
+          {/* Mobile Menu Button */}
+          <div className="md:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="text-gray-700 focus:outline-none text-2xl"
+            >
+              ☰
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      {isOpen && (
+        <div className="md:hidden px-4 pb-4 space-y-2">
+          <a href="#" className="block text-gray-700 hover:text-indigo-600">
+           All Blogs
+          </a>
+          <a href="#" className="block text-gray-700 hover:text-indigo-600">
+            About
+          </a>
+          <a href="#" className="block text-gray-700 hover:text-indigo-600">
+            Contact
+          </a>
+          <button className="w-full bg-indigo-600 text-white px-4 py-2 rounded-lg hover:bg-indigo-700 transition">
+            Get Started
+          </button>
+        </div>
+      )}
+    </nav>
     )
 }
 
--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -3,7 +3,37 @@ const Home=()=>{
     return(
         <>
         <Navbar/>
-        <h1>thi is a demo</h1>
+        <div className="flex min-h-screen ">
+        <div className="w-1/2 p-5">
+            <img 
+            src="/home_images/first_image.jpg"
+            alt="image not found"
+            className="bg-cover h-full"
+            />
+        </div>
+        <div className="w-1/2 p-5 ">
+        <div className="flex justify-center h-full z-30">
+            <div>
+                <h1 className="text-center text-3xl font-sans text-gray-500  py-6">Solve Problam</h1>
+                <img src="/home_images/second_image.jpg"
+                alt="image not found"
+                className="w-[450px]"
+                />
+                <div className="w-[450px]  p-4 hover:text-blue-500 cursor-pointer">
+                    <h1 className="text-3xl py-3 text-gray-500 hover:text-blue-500">Solve the problam in HTML</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, dicta numquam a ea nisi error molestiae facere suscipit nemo ratione mollitia officia eaque vero.</p>
+                </div>
+                <div className="text-center py-10">
+                    <button
+                    className="py-3 px-10 border-1 cursor-pointer hover:text-blue-500"
+                    >All Posts</button>
+                </div>
+            </div>
+        </div>
+        </div>
+      </div>
+
+
         </>
     )
 }
